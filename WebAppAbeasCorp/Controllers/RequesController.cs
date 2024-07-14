a88952c3e82e19d9f7259bea6cc8233d2e4de3d4
jhoancompai <jhoanmanuelsantanagonzalez@gmail.com>
2024-07-14 16:30:35 -0400

    Agregar archivos de proyecto.

---
+++ WebAppAbeasCorp/Controllers/RequesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trabajo_Final___AbeasCorp;
+
+namespace WebAppAbeasCorp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequesController : Controller
+    {
+        private static List<Request> _requests = new List<Request>();
+        private static RequestSchedulerTest _scheduler = new RequestSchedulerTest();
+
+        
+        [HttpPost("NuevaSolicitud")]
+        public IActionResult NuevaSolicitud([FromBody] List<Request> requests)
+        {
+            if (requests == null || requests.Count == 0)
+            {
+                return BadRequest("No se recibieron solicitudes.");
+            }
+
+            foreach (var request in requests) 
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest("Datos de solicitud no validos.");
+                }
+                if (!ModelStateStartime(request.StartTime)) 
+                {
+                    return BadRequest($"La hora de inicio debe estar entre  0 y 23. {request.StartTime} no es valido.");                    
+                }
+                if (!ModelStateDuration(request.Duration))
+                {
+                    return BadRequest($"La duracion de debe estar entre 0 y 15. {request.Duration} no es valido.");
+                }
+                _requests.Add(request);
+            }
+            return Ok("Solicitudes agregadas correctamente.");
+        }
+
+        private bool ModelStateDuration(int startTime)
+        {
+            return startTime >= 0 && startTime <= 23; 
+        }
+
+        private bool ModelStateStartime(int duration)
+        {
+            return duration >= 0 && duration <= 15;
+        }
+        [HttpPost("MaximoBeneficio")]
+        public IActionResult MaximoBeneficio()
+        {
+            if (_requests.Count == 0)
+            {
+                return NotFound("No se recibieron solicitudes.");
+            }
+
+            var maxGain = _scheduler.GetMaxGain(_requests);                  
+            return Ok($"El maximo beneficio es: {maxGain}");
+        }
+    }
+}
